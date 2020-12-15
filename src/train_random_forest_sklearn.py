42bfbcfbc59511347321da98bb6d128686f46ae6
Arkin Mukherjee <arkinmukherjee@gmail.com>
2020-12-15 04:43:45 -0500

    Clean up code

--- src/train_random_forest_sklearn.py
+++ src/train_random_forest_sklearn.py
@@ -1,124 +1,126 @@
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler
-mms = MinMaxScaler()
 from sklearn.metrics import mean_squared_error, r2_score
 import time
 from sklearn.model_selection import KFold
 from sklearn.model_selection import cross_val_score
+from sklearn import linear_model
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import RandomForestRegressor
 
 df = pd.read_csv("../data/processed/bitstampUSD.csv")
-df.head()
 
-train = df[df["Timestamp"]<= 1529899200]
-test = df[df["Timestamp"]> 1529899200]
+print("df.head():")
+print(df.head())
 
-X_train = train.iloc[:,:-1].values
-y_train = train.iloc[:,-1].values
-X_test = test.iloc[:,:-1].values
-y_test = test.iloc[:,-1].values
+train = df[df["Timestamp"] <= 1529899200]
+test = df[df["Timestamp"] > 1529899200]
 
+X_train = train.iloc[:, :-1].values
+y_train = train.iloc[:, -1].values
+X_test = test.iloc[:, :-1].values
+y_test = test.iloc[:, -1].values
+
+mms = MinMaxScaler()
 X_train_scaled = mms.fit_transform(X_train)
 X_test_scaled = mms.transform(X_test)
 
-X_train_scaled
-
-from sklearn import linear_model
+print("X_train_scaled:")
+print(X_train_scaled)
 
 kf = KFold(n_splits=10)
-coef=[]
-MSE =[]
+coef = []
+MSE = []
 for train_index, test_index in kf.split(X_train_scaled):
     Xtrain, Xval = X_train_scaled[train_index], X_train_scaled[test_index]
     ytrain, yval = y_train[train_index], y_train[test_index]
     reg = linear_model.LinearRegression()
-    
+
     start = time.time()
-    reg.fit(Xtrain,ytrain)
-    end = time.time() 
-    print("Time Taken {:.2f}".format(end-start))
+    reg.fit(Xtrain, ytrain)
+    end = time.time()
+    print("Time Taken {:.2f}".format(end - start))
     y_pred_val = reg.predict(Xval)
     mse = mean_squared_error(yval, y_pred_val)
     MSE.append(mse)
     coef.append(reg)
-    
+
 print("Average Error is {:.2f}".format(np.array(MSE).mean()))
 
-alpha=[0.01,0.1,10,100,1000,10000]
+alpha = [0.01, 0.1, 10, 100, 1000, 10000]
 
-MSE=[]
+MSE = []
 for val in alpha:
-    temp=[]
+    temp = []
     for train_index, test_index in kf.split(X_train_scaled):
         Xtrain, Xval = X_train_scaled[train_index], X_train_scaled[test_index]
         ytrain, yval = y_train[train_index], y_train[test_index]
         reg = linear_model.Ridge(alpha=val)
 
-        #start = time.time()
-        reg.fit(Xtrain,ytrain)
-        #end = time.time() 
-        #print("Time Taken {:.2f}".format(end-start))
+        # start = time.time()
+        reg.fit(Xtrain, ytrain)
+        # end = time.time()
+        # print("Time Taken {:.2f}".format(end-start))
         y_pred_val = reg.predict(Xval)
         mse = mean_squared_error(yval, y_pred_val)
         temp.append(mse)
     MSE.append(np.array(temp).mean())
-	
-MSE
 
-MSE=[]
+print("MSE for Ridge:")
+print(MSE)
+
+MSE = []
 for val in alpha:
-    temp=[]
+    temp = []
     for train_index, test_index in kf.split(X_train_scaled):
         Xtrain, Xval = X_train_scaled[train_index], X_train_scaled[test_index]
         ytrain, yval = y_train[train_index], y_train[test_index]
         reg = linear_model.Lasso(alpha=val)
 
-        #start = time.time()
-        reg.fit(Xtrain,ytrain)
-        #end = time.time() 
-        #print("Time Taken {:.2f}".format(end-start))
+        # start = time.time()
+        reg.fit(Xtrain, ytrain)
+        # end = time.time()
+        # print("Time Taken {:.2f}".format(end-start))
         y_pred_val = reg.predict(Xval)
         mse = mean_squared_error(yval, y_pred_val)
         temp.append(mse)
     MSE.append(np.array(temp).mean())
-	
-MSE
 
-from sklearn.preprocessing import PolynomialFeatures
-from sklearn.linear_model import LinearRegression
-from sklearn.pipeline import Pipeline
+print("MSE for Lasso:")
+print(MSE)
 
 kf = KFold(n_splits=5)
-MSE=[]
-for val in [1,2,3,4,5]:
-    temp=[]
+MSE = []
+for val in [1, 2, 3, 4, 5]:
+    temp = []
     for train_index, test_index in kf.split(X_train_scaled):
-        
         trans = PolynomialFeatures(degree=val)
         data = trans.fit_transform(X_train_scaled)
-        
+
         Xtrain, Xval = data[train_index], data[test_index]
         ytrain, yval = y_train[train_index], y_train[test_index]
         reg = linear_model.LinearRegression()
 
-
-        #start = time.time()
-        reg.fit(Xtrain,ytrain)
-        #end = time.time() 
-        #print("Time Taken {:.2f}".format(end-start))
+        # start = time.time()
+        reg.fit(Xtrain, ytrain)
+        # end = time.time()
+        # print("Time Taken {:.2f}".format(end-start))
         y_pred_val = reg.predict(Xval)
         mse = mean_squared_error(yval, y_pred_val)
         temp.append(mse)
     MSE.append(np.array(temp).mean())
-	
-MSE
 
-from sklearn.model_selection import GridSearchCV
-from sklearn.ensemble import RandomForestRegressor
-# Create the parameter grid based on the results of random search 
+print("MSE for Linear:")
+print(MSE)
+
+# Create the parameter grid based on the results of random search
 param_grid = {
     'max_depth': [80, 90, 100, 110],
-    'max_features': [4,5,6,7],
+    'max_features': [4, 5, 6, 7],
     'min_samples_leaf': [3, 4, 5],
     'min_samples_split': [8, 10, 12],
     'n_estimators': [100, 200, 300, 1000]
@@ -126,9 +128,9 @@ param_grid = {
 # Create a based model
 rf = RandomForestRegressor()
 # Instantiate the grid search model
-grid_search = GridSearchCV(estimator = rf, param_grid = param_grid, 
-                          cv = 3, n_jobs = -1, verbose = 2)
-						  
+grid_search = GridSearchCV(estimator=rf, param_grid=param_grid,
+                           cv=3, n_jobs=-1, verbose=2)
+
 grid_search.fit(X_train_scaled, y_train)
 grid_search.best_params_
 {'bootstrap': True,
@@ -142,11 +144,11 @@ best_grid = grid_search.b
 y_pred_test = coef[0].predict(X_test_scaled)
 print('Mean squared error test set: %.2f'
       % mean_squared_error(y_test, y_pred_test))
-	  
+
 y_pred_test = coef[-1].predict(X_test_scaled)
 print('Mean squared error test set: %.2f'
       % mean_squared_error(y_test, y_pred_test))
-	  
+
 y_pred_train = reg.predict(X_train_scaled)
 y_pred_test = reg.predict(X_test_scaled)
 
@@ -156,17 +158,15 @@ print('Mean squared error test set: %.2f'
       % mean_squared_error(y_test, y_pred_test))
 # The coefficient of determination: 1 is perfect prediction
 
-X_train.shape
+print(f"X_train.shape: {X_train.shape}")
 
-reg.coef_
+print(f"reg.coef_: {reg.coef_}")
 
 reg = linear_model.LinearRegression()
-aa = cross_val_score(reg, X_train_scaled, y_train, cv=10 ,scoring = "neg_mean_squared_error")
-
-import numpy as np
+aa = cross_val_score(reg, X_train_scaled, y_train, cv=10, scoring="neg_mean_squared_error")
 
 aa = np.array(aa)
 
-aa = aa*-1
+aa = aa * -1
 
-aa
+print(f"aa: {aa}")
