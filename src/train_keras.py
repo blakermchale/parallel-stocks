dfca676a306179f3e281354b0e73f44c8ac3b37b
Arkin Mukherjee <arkinmukherjee@gmail.com>
2020-12-16 04:07:37 -0500

    Update print statements

--- src/train_keras.py
+++ src/train_keras.py
@@ -13,29 +13,33 @@ import tensorflow as tf
 if __name__ == '__main__':
     df = pd.read_csv("../data/processed/bitstampUSD.csv")
 
+    print("Data Head:")
     print(df.head())
     train_data = df.loc[df["Timestamp"] <= 1529899200]
     test_data = df.loc[df["Timestamp"] > 1529899200]
-    print(train_data.shape)
-    print(test_data.shape)
+    print(f"Train Data Shape: {train_data.shape}")
+    print(f"Test Data Shape: {test_data.shape}")
 
     train_set = train_data.values
-    print(train_set.shape)
+    print(f"Train Set Shape: {train_set.shape}")
 
     sc = MinMaxScaler()
     y_train = train_set[:, -1]
-    X_train = sc.fit_transform(train_set[:, :train_set.shape[1]-1])
+    X_train = sc.fit_transform(train_set[:, :train_set.shape[1] - 1])
     X_train = np.reshape(X_train, (X_train.shape[0], 1, X_train.shape[1]))
 
     test_set = test_data.values
     y_test = test_set[:, -1]
-    X_test = sc.transform(test_set[:, :test_set.shape[1]-1])
+    X_test = sc.transform(test_set[:, :test_set.shape[1] - 1])
     X_test = np.reshape(X_test, (X_test.shape[0], 1, X_test.shape[1]))
 
+    print("X_train.shape, y_train.shape")
     print(X_train.shape, y_train.shape)
+    print("X_test.shape, y_test.shape")
     print(X_test.shape, y_test.shape)
 
     model = Sequential()
+    print(f"type: {type(model)}")
     model.add(LSTM(128, activation="sigmoid", input_shape=(1, 7)))
     model.add(Dropout(0.2))
     model.add(Dense(1))
@@ -44,6 +48,8 @@ if __name__ == '__main__':
         'MeanAbsoluteError'
     ]
     model.compile(loss='mean_squared_error', optimizer='adam', metrics=metrics)
+
+    print("Model Summary:")
     print(model.summary())
 
     start = time()
@@ -59,6 +65,3 @@ if __name__ == '__main__':
     pred_df = pd.DataFrame(y_pred, columns=['Weighted_Price'])
     pred_df.to_csv('../data/predictions/keras_y_pred.csv', index=False)
     print("Done")
-
-
-
